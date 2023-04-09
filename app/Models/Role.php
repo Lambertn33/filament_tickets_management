809e6edb8d77833ad3bae9c7e51725e052500bde
Lambertn33 <lambertgithub@gmail.com>
2023-04-09 10:40:23 +0200

    feat: setup roles and permissions models

---
+++ app/Models/Role.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id', 'title'
+    ];
+
+    /**
+     * The permissions that belong to the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'permission_roles', 'role_id', 'permission_id');
+    }
+
+    /**
+     * The users that belong to the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'role_users', 'role_id', 'user_id');
+    }
+}
